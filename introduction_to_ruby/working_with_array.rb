587420e7ecceb0d7709b29c23e6b0eb9b054f925
emefutty <ekrasnoslobodtseva@bk.ru>
2024-10-04 21:13:25 +0300

    lab 1 task 6
    Добавила в программу два значения аргумента, первое значение говорит, какой из методов задачи выполнить, второй, откуда читать массив(адрес файла)

--- introduction_to_ruby/working_with_array.rb
+++ introduction_to_ruby/working_with_array.rb
@@ -30,3 +30,27 @@ def find_first_positive(array)
   nil # Если положительных элементов нет
 end
 
+# Получаем аргументы программы
+method = ARGV[0]
+filename = ARGV[1]
+
+if File.exist?(filename)
+  # Читаем массив из файла
+  file_content = File.read(filename)
+  array = file_content.split.map(&:to_i)
+else
+  puts "Файл не найден: #{filename}" 
+  exit                             
+end  
+
+  # Выполняем выбранную операцию
+  case method
+  when "min"
+    puts "Минимальный элемент: #{find_min(array)}"
+  when "max"
+    puts "Максимальный элемент: #{find_max(array)}"
+  when "first_positive"
+    puts "Номер первого положительного элемента: #{find_first_positive(array)}"
+  else
+    puts "Неверная операция!"
+  end
