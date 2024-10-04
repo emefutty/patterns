bcd0adba99e17cb89321e49880d2bcde2e934054
emefutty <ekrasnoslobodtseva@bk.ru>
2024-10-04 19:18:16 +0300

    method 3
    Добавила функции которые помогают найти НОД максимального нечетного непростого делителя
    числа и прозведения цифр данного числа.

--- introduction_to_ruby/working_with_numbers.rb
+++ introduction_to_ruby/working_with_numbers.rb
@@ -34,7 +34,57 @@ def product_of_digits_not_divisible_by_five(number)
   return product
 end
 
+
+#3.Найти НОД максимального нечетного непростого делителя числа и прозведения цифр данного числа.
+
+# Функция для вычисления НОД
+def gcd(number1, number2)
+  while number2 != 0
+    number1, number2 = number2, number1 % number2
+  end
+  number1
+end
+
+# Функция для нахождения максимального нечетного непростого делителя
+def max_odd_nonprime_divisor(number)
+  max_divisor = 1
+  (2..number).each do |i|
+    # Проверяем, является ли i делителем и нечетным непростым
+    if number % i == 0 && i % 2 != 0 && !is_prime?(i)
+      if i > max_divisor
+        max_divisor = i 
+      end
+    end
+  end
+  max_divisor
+end
+
+# Функция для вычисления произведения цифр числа
+def product_of_digits(number)
+  product = 1
+  #Пока число существует выполняем алгоритм подсчета
+  while(number != 0) do
+    last_digit = number % 10 #Выбираем последнюю цифру
+    product *= last_digit #умножаем на произведение
+    number /= 10 #Избавляемся от последней цифры числа
+  end
+  return product
+end
+
+# Основная функция, которая находит НОД
+def gcd_of_max_odd_nonprime_and_product(number)
+  max_divisor = max_odd_nonprime_divisor(number)
+  product = product_of_digits(number)
+  
+  # Вычисляем НОД максимального нечетного непростого делителя и произведения цифр
+  gcd_value = gcd(max_divisor, product)
+  
+  return gcd_value
+end
+
+
 puts "Введите число:"
 number = gets.to_i 
+puts "НОД максимального нечетного непростого делителя числа #{number} и произведения цифр данного числа: #{gcd_of_max_odd_nonprime_and_product(number)}"
 puts "Произведение цифр числа #{number}, не делящихся на 5: #{product_of_digits_not_divisible_by_five(number)}"
 puts "Максимальный простой делитель числа #{number}: #{max_prime_divisor(number)}"
