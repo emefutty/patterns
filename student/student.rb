71478579f715e9de3fc3bfbdef79d2eb7624195d
emefutty <ekrasnoslobodtseva@bk.ru>
2024-10-25 20:34:17 +0300

    lab 2 task 3, 4, 5 (краткое написание геттеров и сеттеров)
    Добавила метод attr_accessor для краткого написания геттеров и сеттеров. Обеспечила в конструкторе возможность создания объектов с любой комбинацией заполнения необязательных полей.

--- student/student.rb
+++ student/student.rb
@@ -1,4 +1,5 @@
 class Student
+	attr_accessor :id, :surname, :first_name, :second_name, :phone, :telegram, :mail, :git
 #Функция инициализации
 	def initialize(surname:, first_name:, second_name:, id:nil,
                  phone:nil, telegram:nil, mail:nil, git:nil)
@@ -11,59 +12,9 @@ class Student
     	@mail=mail 
     	@git=git 
 	end
-#Написание геттеров
-  	def id
-  		@id
-  	end
-  	def surname
-  		@surname
-  	end
-  	def first_name
-  		@first_name
-	end
-	def second_name
-  		@second_name
-	end
-	def phone
-		@phone
-	end
-	def telegram
-  		@telegram
-	end
-	def mail
-  		@mail
-	end
-	def git
-  		@git
-  	end
-#Написание сеттеров
-  	def id=(set_id)
-  		@id = set_id
-  	end
-  	def surname=(set_surname)
-  		@surname = set_surname
-  	end
-  	def first_name=(set_first_name)
-  		@first_name = set_first_name
-  	end
-  	def second_name=(set_second_name)
-		@second_name = set_second_name
-	end
-	def phone=(set_phone)
-  		@phone = set_phone
-	end
-	def telegram=(set_telegram)
-	  	@telegram = set_telegram
-	end
-	def mail=(set_mail)
-	  	@mail = set_mail
-	end
-	def git=(set_git)
-		@git = set_git
-	end
 
 	def to_s
-		"ID: #{@id}, ФИО: #{@surname} #{@first_name} #{@second_name},  Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@mail}, GitHub: #{@git}"
+		"ID: #{@id || 'Не указано'}, ФИО: #{@surname} #{@first_name} #{@second_name},  Телефон: #{@phone || 'Не указано'}, Телеграм: #{@telegram || 'Не указано'}, Почта: #{@mail || 'Не указано'}, GitHub: #{@git || 'Не указано'}"
 	end
 
 end
