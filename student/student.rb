e90e3e080d871639b6e22382f9c773e695b4e1a4
emefutty <ekrasnoslobodtseva@bk.ru>
2024-10-25 21:39:43 +0300

    lab 2 task 6 (аргументы конструктора в форме ХЭШа)

--- student/student.rb
+++ student/student.rb
@@ -1,16 +1,17 @@
 class Student
 	attr_accessor :id, :surname, :first_name, :second_name, :phone, :telegram, :mail, :git
 #Функция инициализации
-	def initialize(surname:, first_name:, second_name:, id:nil,
-                 phone:nil, telegram:nil, mail:nil, git:nil)
-    	@surname=surname
-    	@first_name=first_name
-    	@second_name=second_name
-    	@id=id 
-    	@phone=phone 
-    	@telegram=telegram 
-    	@mail=mail 
-    	@git=git 
+	def initialize(args = {})
+		#Проверка обязательных полей
+    	@surname=args[:surname] or raise 'Пожалуйста, введите фамилию'
+    	@first_name=args[:first_name] or raise 'Пожалуйста, введите имя'
+    	@second_name=args[:second_name] or raise 'Пожалуйста, введите отчество'
+    	# Инициализация необязательных полей
+    	@id=args[:id] 
+    	@phone=args[:phone] 
+    	@telegram=args[:telegram] 
+    	@mail=args[:mail] 
+    	@git=args[:git] 
 	end
 
 	def to_s
