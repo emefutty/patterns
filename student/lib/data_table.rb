b30c7b7f2457d660bb9b4c723c0c647c55a0137e
emefutty <ekrasnoslobodtseva@bk.ru>
2025-01-08 01:12:13 +0300

    lab 4 task 2 DataTable metods

--- student/lib/data_table.rb
+++ student/lib/data_table.rb
@@ -1,15 +1,29 @@
 class DataTable
-	private attr_reader :data
+  private attr_reader :data
 
-	def initialize(data)
-		self.data = data
-	end 
+  def initialize(data)
+    self.data = data
+  end 
 
-	private def data=(data)
-		unless data.is_a?(Array) && data.all? { |row| row.is_a?(Array) }
-			raise ArgumentError, "Объект должен являться двумерным массивом"
-		end
-		@data = data
-	end
+  private def data=(data)
+    unless data.is_a?(Array) && data.all? { |row| row.is_a?(Array) }
+      raise ArgumentError, "Объект должен являться двумерным массивом"
+    end
+    @data = data
+  end
 
+  def rows_count
+    data.size
+  end
+
+  def columns_count
+    data[0].size
+  end
+
+  def get_element(row,col)
+    raise IndexError, "Неверный индекс строки" if row < 0 || row >= rows_count
+    raise IndexError, "Неверный индекс столбца" if col < 0 || col >= columns_count
+
+    data[row][col]
+  end
 end
